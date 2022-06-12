19884c609bea9fb74e7b2d95281b523a1db2f37b
dlarocque <daniellrq@outlook.com>
2022-06-12 13:05:01 -0500

    Added business layer to connect to stub database

---
+++ app/src/main/java/com/example/recipe_planner/business/AccessRecipes.java
@@ -0,0 +1,44 @@
+package com.example.recipe_planner.business;
+
+import com.example.recipe_planner.application.Services;
+import com.example.recipe_planner.objects.Recipe;
+import com.example.recipe_planner.persistence.DataAccessStub;
+
+import java.util.List;
+
+public class AccessRecipes {
+    private final DataAccessStub dataAccess;
+
+    public AccessRecipes() {
+        dataAccess = (DataAccessStub) Services.getDataAccess();
+    }
+
+    public List<Recipe> getRecipes(List<Recipe> recipes) {
+        recipes.clear();
+        return dataAccess.getRecipes();
+    }
+
+    public Recipe getRandomRecipe() {
+        return dataAccess.getRandomRecipe();
+    }
+
+    public List<Recipe> getRecipesWithIngredientName(String ingredientName) {
+        return dataAccess.getRecipesWithIngredientName(ingredientName);
+    }
+
+    public List<Recipe> getRecipesWithName(String recipeName) {
+        return dataAccess.getRecipesWithName(recipeName);
+    }
+
+    public void insertRecipe(Recipe recipe) {
+        dataAccess.insertRecipe(recipe);
+    }
+
+    public boolean insertRecipes(List<Recipe> recipes) {
+        return dataAccess.insertRecipes(recipes);
+    }
+
+    public boolean deleteRecipe(Recipe recipe) {
+        return dataAccess.deleteRecipe(recipe);
+    }
+}
