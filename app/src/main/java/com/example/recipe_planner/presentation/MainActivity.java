7517c091f013da684f16e44732093708b42371ac
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2022-07-08 13:57:12 -0500

    add DB to device upon first launch, and a quick db script fix

--- app/src/main/java/com/example/recipe_planner/presentation/MainActivity.java
+++ app/src/main/java/com/example/recipe_planner/presentation/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        copyDatabaseToDevice();
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
