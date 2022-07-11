d8ad18bdfeab4958fb596ef11a8c679468426c88
emersonlesage <emerson.lesage@gmail.com>
2022-07-11 14:35:28 -0500

    added tests for getRecipeIngredients

--- app/src/test/java/com/example/recipe_planner/business/DataAccessTest.java
+++ app/src/test/java/com/example/recipe_planner/business/DataAccessTest.java
@@ -7,6 +7,7 @@ import com.example.recipe_planner.objects.Ingredient;
 import com.example.recipe_planner.objects.Recipe;
 import com.example.recipe_planner.persistence.DataAccess;
 import com.example.recipe_planner.persistence.DataAccessDB;
+import com.example.recipe_planner.persistence.DataAccessStub;
 
 import org.junit.After;
 import org.junit.Before;
@@ -28,10 +29,8 @@ public class DataAccessTest {
     public void setUp() {
         System.out.println("\nStarting Persistence test DataAccess");
 
-
-        dataAccess = new DataAccessDB(Main.dbName);
+        dataAccess = new DataAccessStub(Main.dbName);
         dataAccess.open(Main.getDBPathName());
-
     }
 
     @After
@@ -40,21 +39,42 @@ public class DataAccessTest {
     }
 
     @Test
+    public void testGetIngredientsFromInvalidRecipeIndex(){
+        List<Ingredient> ingredients;
+
+        ingredients = dataAccess.getRecipeIngredients(-1);
+        assertEquals(0, ingredients.size());
+    }
+
+    @Test
     public void testGetDefaultIngredients() {
         List<Ingredient> ingredients;
 
+        double delta = 0.001;
 
+        // get first default recipe
         ingredients = dataAccess.getRecipeIngredients(0);
+        assertNotNull(ingredients);
 
-        for (Ingredient i : ingredients) {
-            System.out.println(i.getName());
-        }
+        // test all ingredients
+        Ingredient ingredient = ingredients.get(0);
+        assertEquals("Balsamic Vinegar", ingredient.getName());
+        assertEquals(0.75, ingredient.getAmount(), delta);
 
-        for (Ingredient i : ingredients) {
-            System.out.println(i.getAmount());
-        }
+        ingredient = ingredients.get(1);
+        assertEquals("Basil Leaves", ingredient.getName());
+        assertEquals(0.25, ingredient.getAmount(), delta);
 
-        System.out.println(ingredients.size());
-        assertEquals("Balsamic Vinegar", ingredients.get(0).getName());
+        ingredient = ingredients.get(2);
+        assertEquals("Olive Oil", ingredient.getName());
+        assertEquals(2.0, ingredient.getAmount(), delta);
+
+        ingredient = ingredients.get(3);
+        assertEquals("Plum Tomatoes", ingredient.getName());
+        assertEquals(4.0, ingredient.getAmount(), delta);
+
+        ingredient = ingredients.get(4);
+        assertEquals("Boneless Skinless Chicken Breast", ingredient.getName());
+        assertEquals(4.0, ingredient.getAmount(), delta);
     }
 }
