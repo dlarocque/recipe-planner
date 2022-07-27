512c50df99877339f95e2d6c8f685f94c3cde68d
dlarocque <daniellrq@outlook.com>
2022-07-26 22:45:58 -0500

    Boilerplate for business persistence seam tests

--- app/src/test/java/com/example/recipe_planner/integration/BusinessPersistenceSeamTest.java
+++ app/src/test/java/com/example/recipe_planner/integration/BusinessPersistenceSeamTest.java
@@ -3,29 +3,22 @@ package com.example.recipe_planner.integration;
 import com.example.recipe_planner.application.Services;
 import com.example.recipe_planner.persistence.DataAccess;
 
-import junit.framework.TestCase;
-
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
-public class BusinessPersistenceSeamTest extends TestCase {
+public class BusinessPersistenceSeamTest {
+    private static final String testDbName = "database/RecipesTest";
     private DataAccess dataAccess;
 
     @Before
     public void setUp() {
-        System.out.println("\nStarting Business Persistence seam test");
-        Services.createDataAccess(); // HSQLDB
+        Services.createDataAccess(testDbName); // HSQLDB
         dataAccess = Services.getDataAccess();
         dataAccess.reset();
     }
 
-    @Test
-    public void TestAccessRecipes() {
-        // Add recipe
-        // Query that recipe exists
-        // Query what ingredients are in the recipe
-        // Modify an ingredient in the recipe
-        // Query that the ingredient row was modified
-        // Query that there are the same number of ingredients
+    @After
+    public void tearDown() {
+        dataAccess.close();
     }
 }
