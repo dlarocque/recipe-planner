395d8e4e95ff2ecccbe3d8d5478026088085b918
dlarocque <daniellrq@outlook.com>
2022-07-27 09:23:33 -0500

    Recipe list tests

--- app/src/androidTest/java/com/example/recipe_planner/AcceptanceTestRunner.java
+++ app/src/androidTest/java/com/example/recipe_planner/AcceptanceTestRunner.java
@@ -1,17 +1,31 @@
 package com.example.recipe_planner;
 
 import com.example.recipe_planner.acceptance.EditRecipeTest;
-import com.example.recipe_planner.acceptance.MealScheduleTest;
+import com.example.recipe_planner.acceptance.RecipeListTest;
 import com.example.recipe_planner.acceptance.ShoppingListTest;
+import com.example.recipe_planner.application.Main;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({MealScheduleTest.class, EditRecipeTest.class, ShoppingListTest.class})
+@Suite.SuiteClasses({RecipeListTest.class, EditRecipeTest.class, ShoppingListTest.class})
 public class AcceptanceTestRunner {
-    public void RunAcceptanceTests() {
-        System.out.println("Running acceptance tests...");
+    private static String testDbPath = "database/RecipesTest";
+    private static String testDbName = "RecipesTest";
+
+    @BeforeClass
+    public static void beginTests() {
+        System.out.println("Running acceptance tests");
+        Main.setDbName(testDbName);
+        Main.setDBPathName(testDbPath);
+    }
+
+    @AfterClass
+    public static void completeTests() {
+        System.out.println("Completed acceptance tests");
     }
 }
 
